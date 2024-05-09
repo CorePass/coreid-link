d2d2db4f7803b56573c868f62e256c626ed3d0a2
Rastislav <rastislav@onion.email>
2024-05-09 18:19:57 +0200

    PWA setup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,13 @@
+<svelte:head>
+	<title>Core ID</title>
+	<meta name="description" content="Create a Core ID connector." />
+	<meta property="og:title" content="Core ID" />
+	<meta property="og:description" content="Create a Core ID connector." />
+	<meta property="og:type" content="website" />
+	<meta name="twitter:title" content="Core ID" />
+	<meta name="twitter:description" content="Create a Core ID connector." />
+</svelte:head>
+
 <div class="divide-y divide-gray-300/50">
 	<h1 class="text-2xl font-bold pb-4 text-black dark:text-white">Core ID</h1>
 	<div class="pt-4 text-slate-500 dark:text-slate-400">
