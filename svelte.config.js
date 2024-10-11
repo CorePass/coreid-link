79082ba64e4ee8a7b0d4f63d6047afb88b822d64
Rastislav <rastislav@onion.email>
2024-10-11 09:09:07 +0200

    Moved

--- svelte.config.js
+++ svelte.config.js
@@ -3,29 +3,10 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Use vitePreprocess as defined by the SvelteKit documentation
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter(),
-
-		// Vite server configuration
-		vite: {
-			server: {
-				proxy: {
-					'/og-image-coreid': {
-						target: process.env.CLOUDFLARE_PAGES_URL,
-						changeOrigin: true,
-						rewrite: (path) => path.replace(/^\/og-image-coreid/, '/functions/og-image-coreid')
-					},
-					'/og-image-intro': {
-						target: process.env.CLOUDFLARE_PAGES_URL,
-						changeOrigin: true,
-						rewrite: (path) => path.replace(/^\/og-image-intro/, '/functions/og-image-intro')
-					}
-				}
-			}
-		}
+		adapter: adapter()
 	}
 };
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,20 @@
-import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		proxy: {
+			'/og-image-coreid': {
+				target: process.env.CLOUDFLARE_PAGES_URL || 'https://coreid.link',
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/og-image-coreid/, '/functions/og-image-coreid')
+			},
+			'/og-image-intro': {
+				target: process.env.CLOUDFLARE_PAGES_URL || 'https://coreid.link',
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/og-image-intro/, '/functions/og-image-intro')
+			}
+		}
+	}
 });
