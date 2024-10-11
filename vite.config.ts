b0def360b2160a287c8b6bb66a4936c7bff4baf3
Rastislav <rastislav@onion.email>
2024-10-11 09:17:13 +0200

    Handle redirects on base level

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,4 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-	server: {
-		proxy: {
-			'/og-image-coreid': {
-				target: process.env.CLOUDFLARE_PAGES_URL || 'https://coreid.link',
-				changeOrigin: true,
-				rewrite: (path) => path.replace(/^\/og-image-coreid/, '/functions/og-image-coreid')
-			},
-			'/og-image-intro': {
-				target: process.env.CLOUDFLARE_PAGES_URL || 'https://coreid.link',
-				changeOrigin: true,
-				rewrite: (path) => path.replace(/^\/og-image-intro/, '/functions/og-image-intro')
-			}
-		}
-	}
 });
